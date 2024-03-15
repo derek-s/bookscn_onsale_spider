97eb5eef8da9072068ed10edc2b01c174f94d277
Derek.S <derekseli@outlook.com>
2024-03-15 12:50:51 +0800

    update

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
 from pymongo import MongoClient
 
 DB_HOSTS = ""
-DB_PORT = 27017
+DB_PORT = 0
 DB_USERNAME = ""
 DB_PWD = ""
 
-client = MongoClient(host=DB_HOSTS, port=DB_PORT, username=DB_USERNAME, password=DB_PWD, authSource="")
-db = client.get_database("")
+client = MongoClient(host=DB_HOSTS, port=DB_PORT, username=DB_USERNAME, password=DB_PWD, authSource="booksChina")
+db = client.get_database("booksChina")
 collection = db.books
--- search.py
+++ search.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
 
 DB_HOSTS = ""
-DB_PORT = 
+DB_PORT = 0
 DB_USERNAME = ""
 DB_PWD = ""
 
-client = MongoClient(host=DB_HOSTS, port=DB_PORT, username=DB_USERNAME, password=DB_PWD, authSource="")
-db = client.get_database("")
+client = MongoClient(host=DB_HOSTS, port=DB_PORT, username=DB_USERNAME, password=DB_PWD, authSource="Library")
+db = client.get_database("Library")
 collection = db.books
 
 
@@ -19,18 +19,28 @@ def SearchBook(isbn, bookname):
         title = bookname.split("：")[0]
     elif("，" in bookname):
         title = bookname.split("，")[0]
+    elif("(" in bookname):
+        title = bookname.replace("(", "")
+        title = title.replace(")", "")
+    elif(")" in bookname):
+        title = bookname.replace("(", "")
+        title = title.replace(")", "")
     else:
         title = bookname
 
 
-    count = collection.count_documents({"ISBN": isbn})
+    ir = collection.count_documents({"ISBN": isbn})
 
-    if(count == 0):
-        r = collection.count_documents({
-            "Title" : {"$regex": title}
+    if(ir == 0):
+        tr = collection.count_documents({
+            "Title" : {"$regex": r"(?i)title"}
         })
 
-    if(count == 0 and r == 0):
-        return True
+    if(ir == 0 and tr == 0):
+        return 0 # 返回0表示库内没有
+    elif(ir == 0 and tr != 0):
+        return 1 # 返回1表示疑似库内有，标题可能匹配
     else:
-        return False
+        return -1 # isbn匹配的情况不管标题如何都认为存在
+    
+
